6fecb4633fe8bf33704663661f28d648a5f8ea1c
asif-karim100 <asif.karim100@gmail.com>
2023-01-29 03:15:34 +0530

    styling using js & jsx

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      {/* <h2>Let's get started </h2> */}
+      <h2>welcome to react  </h2>
+
       <ExpenseItem></ExpenseItem>
 
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate = new Date(2023,2,28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 100
+    const locationOfExpenditure ="Vehicle"
+  
+
   return (
-    <div>
-      <h2>Expense items</h2>
-      <div> Food Rs 10</div>
-      <div>Petrol Rs 100</div>
-      <div>Movies Rs 200</div>
-    </div>
+          <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">Rs{expenseAmount}</div>
+                <div>{locationOfExpenditure}</div>
+            </div>
+          </div>
   );
 }
 
